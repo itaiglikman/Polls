a93610547e78bf23bf8d984e58105437449130bc
itaiglikman <itaig1998@gmail.com>
2025-10-14 21:51:43 +0300

    add types

--- Front/src/utils/types.ts
+++ Front/src/utils/types.ts
@@ -11,3 +11,27 @@ export type NewPoll = {
     choices: string[]
 }
 
+export type FullPoll = {
+    id: number,
+    creator: string,
+    title: string,
+    choices: Choice[],
+}
+
+export type Choice = {
+    id: number,
+    text: string,
+    voteCount: number
+}
+
+export type VoteCount = {
+    id: number,
+    count: number
+}
+
+export type Vote = {
+    pollId: number,
+    username: string,
+    choiceId: number
+}
+
